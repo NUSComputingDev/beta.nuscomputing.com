fa7f354a9183150f7e16d0d20c169a0889ebae71
Nicholas Lum <nicholaslum444@hotmail.com>
2016-07-21 15:03:35 +0800

    start one worker at a time`

--- start_workers.sh
+++ start_workers.sh
@@ -1,4 +1,5 @@
-echo 'starting three workers'
+echo 'Starting one worker'
+echo 'Run this script multiple times to start more workers'
 nohup bundle exec rake resque:work QUEUE=* BACKGROUND=yes &
-nohup bundle exec rake resque:work QUEUE=* BACKGROUND=yes &
-nohup bundle exec rake resque:work QUEUE=* BACKGROUND=yes &
+#nohup bundle exec rake resque:work QUEUE=* BACKGROUND=yes &
+#nohup bundle exec rake resque:work QUEUE=* BACKGROUND=yes &
