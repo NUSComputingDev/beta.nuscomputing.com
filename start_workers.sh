91d7cb74b9c0c60804fe347f18b9a0e5eb2ebab7
Nicholas Lum <nicholaslum444@hotmail.com>
2016-07-21 13:53:30 +0800

    ensure resque workers write to nohup

--- start_workers.sh
+++ start_workers.sh
@@ -1,1 +1,4 @@
-bundle exec rake resque:workers QUEUE=* COUNT=3 BACKGROUND=yes
+echo 'starting three workers'
+nohup bundle exec rake resque:work QUEUE=* BACKGROUND=yes &
+nohup bundle exec rake resque:work QUEUE=* BACKGROUND=yes &
+nohup bundle exec rake resque:work QUEUE=* BACKGROUND=yes &
